8fb21cf98b6e2e0659717f863c05cd9b60f26f07
Lokesh <23ca1031@mvn.edu.in>
2025-09-10 22:31:42 +0530

    Update bot_commands.py

--- bot/helper/telegram_helper/bot_commands.py
+++ bot/helper/telegram_helper/bot_commands.py
@@ -41,8 +41,8 @@ class _BotCommands:
         self.EvalCommand = f'eval{CMD_SUFFIX}'
         self.ExecCommand = f'exec{CMD_SUFFIX}'
         self.ClearLocalsCommand = f'clearlocals{CMD_SUFFIX}'
-        self.BotSetCommand = [f'bsetting1{CMD_SUFFIX}', f'bs{CMD_SUFFIX}']
-        self.UserSetCommand = [f'usetting1{CMD_SUFFIX}', f'us{CMD_SUFFIX}']
+        self.BotSetCommand = [f'bsetting1{CMD_SUFFIX}', f'bs1{CMD_SUFFIX}']
+        self.UserSetCommand = [f'usetting1{CMD_SUFFIX}', f'us1{CMD_SUFFIX}']
         self.BtSelectCommand = f'btsel{CMD_SUFFIX}'
         self.CategorySelect = f'ctsel{CMD_SUFFIX}'
         self.SpeedCommand = [f'speedtest{CMD_SUFFIX}', f'sp{CMD_SUFFIX}']
